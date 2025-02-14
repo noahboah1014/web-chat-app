5ce4747c1bc6f232675042005afb3a029577a5d5
noahboah1014 <noahboah1014@gmail.com>
2025-02-14 10:29:06 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,6 @@
-// Initialize Supabase client
-const supabase = window.supabase.createClient(
-    "https://pdtudwscpnptuhbhkcgo.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkdHVkd3NjcG5wdHVoYmhrY2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0NTQ0MDcsImV4cCI6MjA1NTAzMDQwN30.8GHhqN217OOUNUfBSB-TH7Kl9XIexkkRwxBoz_z_xB0"
-);
+const { createClient } = supabase;
+
+const SUPABASE_URL = "https://pdtudwscpnptuhbhkcgo.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkdHVkd3NjcG5wdHVoYmhrY2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0NTQ0MDcsImV4cCI6MjA1NTAzMDQwN30.8GHhqN217OOUNUfBSB-TH7Kl9XIexkkRwxBoz_z_xB0";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
